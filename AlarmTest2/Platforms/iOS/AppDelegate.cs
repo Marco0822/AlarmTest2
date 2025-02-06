3b183b5b3a3f727d6f14e323e461b4ca33f7c14e
Marco Kwong <ckwong04@students.cs.ubc.ca>
2025-02-06 10:49:31 -0800

    Initial Commit

---
+++ AlarmTest2/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AlarmTest2;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
